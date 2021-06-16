71f0125e112c1312065d37d425f4cf181465e55d
Thomas Rolison <thomas.rolison@gmail.com>
2021-06-15 23:28:52 -0600

    Initial check-in of module ConsoleApp1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doAnother;
+            do
+            {
+                try
+                {
+                    Console.Write("Please enter asocial security number: ");
+                    String userInput = Console.ReadLine();
+                    SocialSecurityNumber ssn = new SocialSecurityNumber();
+                    ssn.SSN = userInput;
+                }
+                catch(SSNFormatException exc)
+                {
+                    Console.WriteLine("Please re-enter social security number in correct format.");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("There was an error! " + exc.Message + "\nException type: " + exc.GetType());
+                }
+                finally
+                {
+                    Console.WriteLine("This section always executes!");
+                }
+
+                Console.Write("Do another (y/n): ");
+                doAnother = Console.ReadLine();
+            } while (doAnother == "y");
+        }
+    }
+}
---
+++ SocialSecurityNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SocialSecurityNumber
+    {
+        private string ssn;
+
+        public string SSN
+        {
+            get { return ssn; }
+            set
+            {
+                string pattern = @"^\d{3}-\d{2}-\d{4}$";
+
+                if (Regex.IsMatch(value, pattern))
+                {
+                    ssn = value;
+                }
+                else
+                {
+                    throw new SSNFormatException();
+                }
+            }
+        }
+    }
+}
